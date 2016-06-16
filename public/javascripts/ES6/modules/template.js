38f9c2c15128187de81b4011c0f4612c62ef2362
Julien Fauconnier <fauconnier.julien@outlook.com>
2016-06-16 18:49:23 +0200

    Add JSON transformation

--- public/javascripts/ES6/modules/template.js
+++ public/javascripts/ES6/modules/template.js
@@ -1,79 +1,110 @@
+import * as resp from "./responsive";
+
 export {exportTemplate, importTemplate};
 
-function getPrevious(element) {
-  let prev_el = element.previousSibling;
-  while (prev_el.nodeType == 3) {
-    prev_el = prev_el.previousSibling;
-  }
-  return prev_el;
-}
-
-function recursive(element) {
-  //var classname = element.className.split(' ');
-  // element.nodeName == 'UL'
-  let Result = {"title": '', "json": {}};
-  let json = {};
-  let cur_json_key = '';
-  if (element.nodeType == 3) {
-
-  }
-  else {
-    //console.log( element.nodeType, element );
-
-    var nodeName = element.nodeName.toLowerCase();
-    var nodeClass = element.className.toLowerCase();
-
-    // if this is the SPAN with class 'TITLE', then create an object with the innerHTML as KEY
-    // and later the value should be another object, returned from the recursion...
-    if (nodeName == 'div') {
-      json[element.innerHTML] = {};
-      Result.title = element.innerHTML;
-      Result.json = json;
-    }
-    else {
-      var is_title_found = 0;
-      var title_found = '';
-      var res = {};
-      // go deeper
-      for (var child = 0; child < element.childNodes.length; child++) {
-        //json = $.extend( {}, recursive( element.childNodes[child] ));
-        res = recursive(element.childNodes[child]);
-        if (res) {
-          if (res.title != '') {
-            is_title_found = 1;
-            title_found = res.title;
-          }
-          else {
-            $.extend(true, json, res.json);
-          }
-          console.log(JSON.stringify(json));
-        }
-      }
-      if (title_found) {
-        Result.json[title_found] = json
-      }
-      else {
-        Result.json = json;
-      }
-    }
-    return Result;
-  }
-}
-
-function exportTemplate(nodes) {
+function exportTemplate(node) {
   /*
    jQuery.each(nodes, (index, node) => {
    window.console.log(node);
    });
    */
 
-  let objectToJSON = recursive(nodes[0]);
+  //let test = toJSON(node);
 
-  let finalJSON = JSON.stringify(objectToJSON, null, 2);
+  //let finalJSON = JSON.stringify(test, null, 2);
 
-  window.console.log(finalJSON);
+  console.log(JSON.stringify(toJSON(node)));
+  //window.console.log(finalJSON);
 }
 
 function importTemplate() {
   alert("Test");
+}
+
+
+function toJSON(node) {
+  node = node || this;
+
+  console.log(node);
+
+  let obj = {};
+  let nodeType;
+  let nodeSize = false;
+
+  if ($(node).hasClass("editable")) {
+    nodeType = "container";
+  }
+  else if ($(node).hasClass("row")) {
+    nodeType = "row";
+  }
+  else if ($(node).hasClass("columns")) {
+    nodeType = "columns";
+    nodeSize = true;
+  }
+  else {
+    nodeType = $(node).prop("tagName");
+  }
+
+  if (nodeSize) {
+    let size = resp.getColumnSize($(node));
+    obj.small = $(node).attr("height");
+    obj.large = size.large;
+  }
+
+  let childNodes = $(node).children();
+
+  if (childNodes.length > 0) {
+    obj[nodeType] = [];
+    jQuery.each(childNodes, (index, childNode) => {
+      if ($(node).not(".js-off-canvas-exit"))
+        obj[nodeType].push(toJSON(childNode));
+    });
+  }
+  else {
+    console.log(node);
+    obj[nodeType] = $(node).val();
+  }
+
+  return obj;
+}
+
+function toDOM(obj) {
+  if (typeof obj == 'string') {
+    obj = JSON.parse(obj);
+  }
+  var node, nodeType = obj.nodeType;
+  switch (nodeType) {
+    case 1: //ELEMENT_NODE
+      node = document.createElement(obj.tagName);
+      var attributes = obj.attributes || [];
+      for (var i = 0, len = attributes.length; i < len; i++) {
+        var attr = attributes[i];
+        node.setAttribute(attr[0], attr[1]);
+      }
+      break;
+    case 3: //TEXT_NODE
+      node = document.createTextNode(obj.nodeValue);
+      break;
+    case 8: //COMMENT_NODE
+      node = document.createComment(obj.nodeValue);
+      break;
+    case 9: //DOCUMENT_NODE
+      node = document.implementation.createDocument();
+      break;
+    case 10: //DOCUMENT_TYPE_NODE
+      node = document.implementation.createDocumentType(obj.nodeName);
+      break;
+    case 11: //DOCUMENT_FRAGMENT_NODE
+      node = document.createDocumentFragment();
+      break;
+    default:
+      return node;
+  }
+  if (nodeType == 1 || nodeType == 11) {
+    var childNodes = obj.childNodes || [];
+    for (i = 0, len = childNodes.length; i < len; i++) {
+      node.appendChild(toDOM(childNodes[i]));
+    }
+  }
+  return node;
 }
