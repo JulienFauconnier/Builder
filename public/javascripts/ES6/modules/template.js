9dc9ccc3e891af14b2fbaaa6c82277dc7ca4a60f
Julien Fauconnier <fauconnier.julien@outlook.com>
2016-06-17 15:47:12 +0200

    JSON Transformation in Progress

--- public/javascripts/ES6/modules/template.js
+++ public/javascripts/ES6/modules/template.js
@@ -47,13 +47,13 @@ function toJSON(node) {
 
   if (nodeSize) {
     let size = resp.getColumnSize($(node));
-    obj.small = $(node).attr("height");
-    obj.large = size.large;
+    obj.small = parseInt(size.small.slice(6, size.small.length));
+    obj.large = parseInt(size.large.slice(6, size.large.length));
   }
 
   let childNodes = $(node).children();
 
-  if (childNodes.length > 0) {
+  if (childNodes.length > 0 && nodeType !== "P") {
     obj[nodeType] = [];
     jQuery.each(childNodes, (index, childNode) => {
       if ($(node).not(".js-off-canvas-exit"))
@@ -61,8 +61,12 @@ function toJSON(node) {
     });
   }
   else {
-    console.log(node);
-    obj[nodeType] = $(node).val();
+    if (nodeType === "P") {
+      obj[nodeType] = $(node).html();
+    }
+    else {
+      obj[nodeType] = $(node).text();
+    }
   }
 
   return obj;
@@ -108,3 +112,72 @@ function toDOM(obj) {
   }
   return node;
 }
+
+/**
+ *
+ */
+function reloadCSS() {
+  $("#result").contents().find("*").removeAttr("style");
+  for (let init = 0; init < 3; init++) {
+    for (let target in data['children']) {
+      if ((!target.match("^.") && !target.match("^#") && init == 0) || (target.match("^.") && init == 1) || (target.match("^#") && init == 2))
+        for (let cssName in data['children'][target]['attributes'])
+          $("#result").contents().find(target).css(cssName, data['children'][target]['attributes'][cssName]);
+    }
+  }
+}
+
+/**
+ *
+ */
+$("#submit").on("click", function () {
+  $.post("setStyle.php", {
+    'data': process(CSSJSON.toCSS(data))
+  });
+  alert("CSS Applied");
+});
+
+/**
+ *
+ */
+$("#prod").on("click", function () {
+  let confirmation = confirm('Dialogue');
+
+  if (confirmation) {
+    $.post("setProd.php");
+    alert("CSS now in Prod");
+  } else {
+    alert("Operation Cancelled");
+  }
+});
+
+/**
+ *
+ */
+$("#export").on("click", function () {
+  let a = document.createElement('a');
+  let now = new Date(Date.now());
+  let options = {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+  };
+
+  a.setAttribute('download', document.domain + '_' + now.toLocaleDateString('fr-FR', options) + '.json');
+  a.href = 'data:application/json,' + JSON.stringify(data);
+  a.innerHTML = 'testing';
+  a.style.display = 'none';
+  document.body.appendChild(a);
+
+  a.click();
+});
+
+/**
+ *
+ */
+$("#import").on("click", function () {
+  $("#fileInput").click();
+});
