4db54fbe45db345f3513b072fe17b80c7c9b1eab
Julien Fauconnier <fauconnier.julien@outlook.com>
2016-07-04 16:17:27 +0200

    Toolbox is now a stateless plugin

--- public/javascripts/ES6/app.js
+++ public/javascripts/ES6/app.js
@@ -7,18 +7,17 @@ $(document).foundation();
 /**
  * Selection of UI parts
  */
-const leftPanel = $(".off-canvas .position-left");
+//const leftPanel = $(".off-canvas .position-left");
 const rightContent = $(".off-canvas-content");
 
 /**
  * Declaration and Initialization of widgets requirement
  * @type {jQuery|HTMLElement}
  */
-const toolbox = leftPanel;
 const dropZone = $("<div>", {class: "drop-zone"}).appendTo(rightContent);
 
 /**
  * Widget Loading
  */
 DropZone(dropZone);
-Toolbox(toolbox);
+Toolbox.init(dropZone);
--- public/javascripts/ES6/modules/toolbox.js
+++ public/javascripts/ES6/modules/toolbox.js
@@ -1,73 +1,82 @@
 import Tool from "./tool";
 import * as template from "./template";
 
-/**
- * Initialize Toolbox widget (fill left column with blocks)
- */
-function initModels() {
-  const components = Tool.initComponents();
-  const structures = Tool.initStructures(components);
-  const groups = Tool.initGroups(components, structures);
+const toolbox = ((() => {
+  /**
+   * Initialize Toolbox widget (fill left column with blocks)
+   */
+  let container;
 
-  jQuery.each(components, (index, component) => {
-    let li = $("<li>");
-    li.data("data", component.toHTML());
-    let span = $("<span>", {class: "draggable-model"});
-    span.text(component.name);
-    li.append(span);
-    $(".componentsContainer").append(li);
-  });
+  const initModels = () => {
+    const components = Tool.initComponents();
+    const structures = Tool.initStructures(components);
+    const groups = Tool.initGroups(components, structures);
 
-  jQuery.each(structures, (index, structure) => {
-    let li = $("<li>");
-    li.data("data", structure.toHTML());
-    let span = $("<span>", {class: "draggable-model"});
-    span.text(structure.name);
-    li.append(span);
-    $(".structuresContainer").append(li);
-  });
+    jQuery.each(components, (index, component) => {
+      const li = $("<li>");
+      li.data("data", component.toHTML());
+      const span = $("<span>", {class: "draggable-model"});
+      span.text(component.name);
+      li.append(span);
+      $(".componentsContainer").append(li);
+    });
 
-  jQuery.each(groups, (index, group) => {
-    let li = $("<li>");
-    li.data("data", group.toHTML());
-    let span = $("<span>", {class: "draggable-model"});
-    span.text(group.name);
-    li.append(span);
-    $(".groupsContainer").append(li);
-  });
+    jQuery.each(structures, (index, structure) => {
+      const li = $("<li>");
+      li.data("data", structure.toHTML());
+      const span = $("<span>", {class: "draggable-model"});
+      span.text(structure.name);
+      li.append(span);
+      $(".structuresContainer").append(li);
+    });
 
-  return $(".componentsContainer, .structuresContainer, .groupsContainer").children();
-}
+    jQuery.each(groups, (index, group) => {
+      const li = $("<li>");
+      li.data("data", group.toHTML());
+      const span = $("<span>", {class: "draggable-model"});
+      span.text(group.name);
+      li.append(span);
+      $(".groupsContainer").append(li);
+    });
 
-/**
- *
- */
-export default function initToolbox() {
-  const dropZone = $(".drop-zone");
-  const Models = initModels();
+    container = $(".componentsContainer, .structuresContainer, .groupsContainer").children();
+  };
 
-  Models.draggable({
-    opacity: 0.5,
-    appendTo: dropZone,
-    containment: dropZone,
-    helper: "clone",
-    start() {
-      dropZone.dropzone('initDroppables');
-    },
-    stop() {
-      dropZone.find(".droppables-container").remove();
-      dropZone.find(".droppables-container-nested").remove();
-    }
-  });
+  /**
+   *
+   */
+  const initToolbox = (dropZone) => {
+    initModels();
 
-  $(".selector").on("change", () => {
-  });
+    container.draggable({
+      opacity: 0.5,
+      appendTo: dropZone,
+      containment: dropZone,
+      helper: "clone",
+      start() {
+        dropZone.dropzone('initDroppables');
+      },
+      stop() {
+        dropZone.find(".droppables-container").remove();
+        dropZone.find(".droppables-container-nested").remove();
+      }
+    });
 
-  $(".submit-template").on("click", () => {
-    template.exportTemplate($(".drop-zone"));
-  });
+    $(".selector").on("change", () => {
+    });
 
-  $(".set-template").on("click", () => {
-    template.importTemplate();
-  });
-}
+    $(".submit-template").on("click", () => {
+      template.exportTemplate($(".drop-zone"));
+    });
+
+    $(".set-template").on("click", () => {
+      template.importTemplate();
+    });
+  };
+
+  return {
+    init: initToolbox
+  };
+}))();
+
+export default toolbox;
